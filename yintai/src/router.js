2259974b2f02fafeee3e7b361599f00566b10ec0
https://git.sinacloud.com/wangtest <1310053687@qq.com>
2017-09-03 14:31:06 +0800

    add file

---
+++ yintai/src/router.js
@@ -0,0 +1,12 @@
+import {
+	HashRouter as Router,
+	Route,
+	Redirect,
+	Link,
+	Switch	
+} from 'react-router-dom';
+import React from "react";
+import App from "./components/App";
+import Home from "./Components/Home"; 
+import Library from "./Components/Library"; 
+import My from "./Components/My";  
